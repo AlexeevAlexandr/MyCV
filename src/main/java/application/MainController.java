2ad8b17ceb3b11d355324239edd03f1f9e2692a2
AlexeevAlexandr <https://github.com/AlexeevAlexandr>
2018-10-05 09:44:55 +0300

    my cv

---
+++ src/main/java/application/MainController.java
@@ -0,0 +1,15 @@
+package application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = { "/", "/start" })
+    public String start() {
+        return "start";
+    }
+}
