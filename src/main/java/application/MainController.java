eddc3221812ef3e6241720177a549de81fc26964
Alexandr Alexeev <fart82@ukr.net>
2018-10-06 21:14:18 +0300

    cv

--- src/main/java/application/MainController.java
+++ src/main/java/application/MainController.java
@@ -1,25 +1,28 @@
 package application;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-
-    @RequestMapping(value = { "/my_cv" })
-    public String my_cv() {
-        return "my_cv";
-    }
 
     @RequestMapping(value = { "/", "/start_page" })
     public String MyCV() {
         return "start_page";
     }
 
-    @RequestMapping(value = { "/", "/proposals" })
+    @RequestMapping(value = { "/my_cv" })
+    public String my_cv() {
+        return "my_cv";
+    }
+
+    @RequestMapping(value = { "/proposals" })
     public String proposals() {
         return "proposals";
+    }
+
+    @RequestMapping(value = { "/page" })
+    public String page() {
+        return "page";
     }
 }
